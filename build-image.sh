960abb793fcc8ad5d1d83aaf49d35f3639e76102
Denis <benato.denis96@gmail.com>
2024-03-05 23:53:17 +0100

    Add user to more groups

--- build-image.sh
+++ build-image.sh
@@ -125,7 +125,7 @@ passwd --lock root
 
 # create user
 groupadd -r autologin
-useradd -m ${USERNAME} -G autologin,wheel,audio,video,realtime
+useradd -m ${USERNAME} -G autologin,wheel,audio,video,realtime,plugdev,kvm,adm,log,rfkill,uucp 
 echo "${USERNAME}:${USERNAME}" | chpasswd
 
 # set the default editor, so visudo works
