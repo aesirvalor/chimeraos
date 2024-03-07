7d93f3e321c0fb69beb4419bb96f460f7e8dfe72
Denis <benato.denis96@gmail.com>
2024-03-07 03:38:55 +0100

    Remove plugdev

--- build-image.sh
+++ build-image.sh
@@ -132,7 +132,7 @@ passwd --lock root
 
 # create user
 groupadd -r autologin
-useradd -m ${USERNAME} -G autologin,wheel,audio,video,realtime,plugdev,kvm,adm,log,rfkill,uucp 
+useradd -m ${USERNAME} -G autologin,wheel,audio,video,realtime,kvm,adm,log,rfkill,uucp 
 echo "${USERNAME}:${USERNAME}" | chpasswd
 
 # set the default editor, so visudo works
