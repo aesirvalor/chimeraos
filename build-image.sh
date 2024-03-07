c7619c3c30d3154ffd9988a829e5e0fec653f42e
Denis <benato.denis96@gmail.com>
2024-03-07 21:49:46 +0100

    Add software

--- build-image.sh
+++ build-image.sh
@@ -137,7 +137,7 @@ passwd --lock root
 
 # create user
 groupadd -r autologin
-useradd -m ${USERNAME} -G autologin,wheel,audio,video,realtime,kvm,adm,log,rfkill,uucp 
+useradd -m ${USERNAME} -G autologin,wheel,audio,video,realtime,kvm,adm,log,rfkill,uucp,plugdev
 echo "${USERNAME}:${USERNAME}" | chpasswd
 
 # set the default editor, so visudo works
